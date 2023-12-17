0e92a4a9f55fa1a690f1ac7703f629adc92a5961
sylvainSUPINTERNET <sylvain.joly00@gmail.com>
2023-12-17 22:48:00 +0100

    [WIP]

--- Services/LinkService.cs
+++ Services/LinkService.cs
@@ -13,8 +13,10 @@ public class LinkService : ILinkService
 
     public LinksDto SaveLink()
     {
-        this._logger.LogInformation("SaveLink");
+        _logger.LogInformation("SaveLink");
 
+        // TODO: https://github.com/sylvainSUPINTERNET/zerecruteur-service/blob/master/src/services/product.service.ts
+        // TODO: difference c'est qu'il faut utiliser sub du claims pour identifier l'utilisateur !
         return new LinksDto
         {
             Id = "123",
